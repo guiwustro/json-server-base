1d04b50ff0e0cf3ca2fdf9202b435a8d86a5dee2
Guilherme Wustro <gui.wustro@gmail.com>
2022-08-23 15:40:12 -0300

    API Finalizada

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const router = jsonServer.router("db.json");
 app.db = router.db;
 
 const rules = auth.rewriter({
-  users: 600,
+	users: 600,
+	exams: 664,
+	vaccines: 664,
 });
 
 app.use(cors());
